53c4ae105025308eb62d9b6209bf6724f49a42b7
Sujeeth-Varma <sujeethvarma27@gmail.com>
2026-01-16 23:29:36 +0530

    init application.yml

---
+++ src/main/java/in/sujeeth/search_engine_mcp/SearchEngineMcpApplication.java
@@ -0,0 +1,13 @@
+package in.sujeeth.search_engine_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngineMcpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/sujeeth/search_engine_mcp/SearchEngineMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sujeeth.search_engine_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
